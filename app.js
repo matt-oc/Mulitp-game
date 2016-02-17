fc4e44cad25fe5da05c7439b258af04ac40dc32c
Matthew O'Connor <matthewocsecond@gmail.com>
2016-02-17 09:55:30 +0000

    initial html page

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var path = require('path');
 
-app.get.('/', function(req, res){
-  res.sendFile(__dirname + 'index.html')
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 http.listen(3000, function(){
