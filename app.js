e6618c1a3c1af7a59d027a1a61867f1984e62803
Matthew O'Connor <matthewocsecond@gmail.com>
2016-02-17 10:57:36 +0000

    creating and removing players

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+var world = require('./js/server_world');
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -12,12 +13,33 @@ app.get('/js/client_world.js', function(req, res){
 });
 
 io.on('connection', function(socket){
-  console.log('A user connected');
-  
-  socket.on('disconnect', function(){
-    console.log('A user disconnected')
-  })
-})
+    console.log('a user connected');
+
+    var id = socket.id;
+    world.addPlayer(id);
+
+    var player = world.playerForId(id);
+    socket.emit('createPlayer', player);
+
+    socket.broadcast.emit('addOtherPlayer', player);
+
+    socket.on('requestOldPlayers', function(){
+        for (var i = 0; i < world.players.length; i++){
+            if (world.players[i].playerId != id)
+                socket.emit('addOtherPlayer', world.players[i]);
+        }
+    });
+    socket.on('updatePosition', function(data){
+        var newData = world.updatePlayerData(data);
+        socket.broadcast.emit('updatePosition', newData);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+        io.emit('removeOtherPlayer', player);
+        world.removePlayer( player );
+    });
+
+});
 http.listen(3000, function(){
   console.log('Server listening on port 3000')
 })
