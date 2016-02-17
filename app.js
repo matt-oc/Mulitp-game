54cad2b5ed67a2c6c6d63a055cb2ddcd4cfea242
Matthew O'Connor <matthewocsecond@gmail.com>
2016-02-17 10:12:38 +0000

    checking for connect and disconnect

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+io.on('connection', function(socket){
+  console.log('A user connected');
+  
+  socket.on('disconnect', function(){
+    console.log('A user disconnected')
+  })
+})
 http.listen(3000, function(){
   console.log('Server listening on port 3000')
 })
